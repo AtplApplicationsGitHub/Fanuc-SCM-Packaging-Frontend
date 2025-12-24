13d1f4661fefe5b3368adcecf30f0274b111e003
AtplApplicationsGitHub <66077941+AtplApplicationsGitHub@users.noreply.github.com>
2025-12-24 15:32:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
- 
+set -e
+
+# Inject the environment variable into a JS file for the frontend
 echo "window._env_ = {
   \"VITE_API_BASE_URL\": \"$VITE_API_BASE_URL\"
 };" > /usr/share/nginx/html/env-config.js
- 
-# Start Nginx
-exec "$@
